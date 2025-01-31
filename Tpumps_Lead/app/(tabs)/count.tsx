24460ee04c24b8799334334ab593148616a44325
szserrano <szserrano@mail.csuchico.edu>
2025-01-31 00:50:32 -0800

    Finished Count Page. Move to PR and continue to Report

--- Tpumps_Lead/app/(tabs)/count.tsx
+++ Tpumps_Lead/app/(tabs)/count.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, TextInput, Button, Image, Platform } from 'react-native';
+import { StyleSheet, TextInput, Button, KeyboardAvoidingView, Alert, Platform, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import SelectDropdown from 'react-native-select-dropdown';
 
 import { NumericFormat } from 'react-number-format';
@@ -18,11 +18,31 @@ export default function TabFourScreen() {
   const [number5s, setNumber5s] = useState('');
   const [number1s, setNumber1s] = useState('');
   const [numberCent, setNumberCent] = useState('');
+  const [numberQs, setNumberQ] = useState('');
+  const [numberDs, setNumberD] = useState('');
+  const [numberNs, setNumberN] = useState('');
   const [selectedItem, setSelectedItem] = useState('');
   
   const [total, setTotal] = useState(0);
-  
+
   const [isOpen, setIsOpen] = useState(false);
+
+  const registerItems = [
+    {value: number20s, multiplier: 20},
+    {value: number10s, multiplier: 10}, 
+    {value: number5s, multiplier: 5}, 
+    {value: number1s, multiplier: 1}, 
+    {value: numberCent, multiplier: 1}
+  ];
+  const safeItems = [
+    {value: number20s, multiplier: 20},
+    {value: number10s, multiplier: 10}, 
+    {value: number5s, multiplier: 5}, 
+    {value: number1s, multiplier: 1}, 
+    {value: numberQs, multiplier: 10}, 
+    {value: numberDs, multiplier: 5}, 
+    {value: numberNs, multiplier: 2}
+  ];
 
   const itemToCount = [
     {title: 'safe'},
@@ -30,129 +50,151 @@ export default function TabFourScreen() {
   ];
 
   const calculateTotal = () => {
-    const result = (parseInt(number20s) * 20) + (parseInt(number10s) * 10) + (parseInt(number5s) * 5) + parseInt(number1s) + parseFloat(numberCent);
-    setTotal(result);
+    setTotal(0);
+    {/* This does different calculations if the user is counting the register or the safe. If user hasn't selected what they are counting, total will default to 0 */}
+    if (selectedItem === "register") { 
+      for (let i = 0; i < registerItems.length; i++){
+        if (registerItems[i].value !== ''){
+          setTotal(total => total + (parseFloat(registerItems[i].value) * registerItems[i].multiplier));
+        } 
+      }
+    } else if (selectedItem === "safe"){
+      for (let i = 0; i < safeItems.length; i++){
+        if (safeItems[i].value !== ''){
+          setTotal(total => total + (parseInt(safeItems[i].value) * safeItems[i].multiplier));
+        } 
+      }
+    } else {
+      Alert.alert("Error", "Please select what you are counting before pressing Calculate.");
+    }
   };
 
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#38761d' }}
-      headerImage={
-        <IconSymbol
-          size={310}
-          color="#b6d7a8"
-          name="dollarsign"
-          style={styles.headerImage}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Count the Register</ThemedText>
-      </ThemedView>
-      <ThemedText>This page will include the functionality to help you count your register. Please select if you are counting the register or safe and enter the counts of bills and coins below:</ThemedText>
-      <ThemedView style={styles.billRowContainer}>
-        <SelectDropdown
-          data={itemToCount}
-          onSelect={(selectedItem, index) => {
-            console.log(selectedItem, index);
-            setSelectedItem(selectedItem);
-          }}
-          renderButton={(selectedItem, isOpen) => {
-            return (
-              <ThemedView style={styles.dropdownButtonStyle}>
-                <ThemedText style={styles.dropdownButtonTxtStyle}>{selectedItem !== null ? selectedItem.title : 'What are you counting? (Tap Me)'}</ThemedText>
-              </ThemedView>
-            );
-          }}
-          renderItem={(item, index, isSelected) => {
-            return (
-              <ThemedView
-                style={{
-                  ...styles.dropdownItemStyle,
-                  ...(isSelected && {backgroundColor: '#D2D9DF'}),
-                }}>
-                <ThemedText style={styles.dropdownItemTxtStyle}>{item.title}</ThemedText>
-              </ThemedView>
-            );
-          }}
-          dropdownStyle={styles.dropdownMenuStyle}
-        />
-      </ThemedView>
-      <ThemedView style={styles.billRowContainer}>
-      <ThemedView style={styles.inputBox}>
-          <TextInput 
-              keyboardType='numeric' 
-              value={number20s} 
-              onChangeText={text => setNumber20s(text)} 
-              placeholder="How many $20 bills?"
-              placeholderTextColor='grey'
-          />
-        </ThemedView>
-        <ThemedView style={styles.multiplyBox}>
-          <ThemedText>x $20 = </ThemedText>
-        </ThemedView>
-        <ThemedView style={styles.outputBox}>
-          <ThemedText>{number20s !== '' ? parseInt(number20s) * 20 : 0}</ThemedText>
-        </ThemedView>
-      </ThemedView>
-      <ThemedView style={styles.billRowContainer}>
-        <ThemedView style={styles.inputBox}>
-          <TextInput 
-              keyboardType='numeric' 
-              value={number10s} 
-              onChangeText={text => setNumber10s(text)} 
-              placeholder="How many $10 bills?"
-              placeholderTextColor='grey'
-          />
-        </ThemedView>
-        <ThemedView style={styles.multiplyBox}>
-          <ThemedText>x $10 = </ThemedText>
-        </ThemedView>
-        <ThemedView style={styles.outputBox}>
-          <ThemedText>{number10s !== '' ? parseInt(number10s) * 10 : 0}</ThemedText>
-        </ThemedView>
-      </ThemedView>
-      <ThemedView style={styles.billRowContainer}>
-      <ThemedView style={styles.inputBox}>
-          <TextInput 
-              keyboardType='numeric' 
-              value={number5s} 
-              onChangeText={text => setNumber5s(text)} 
-              placeholder="How many $5 bills?"
-              placeholderTextColor='grey'
-          />
-        </ThemedView>
-        <ThemedView style={styles.multiplyBox}>
-          <ThemedText>x $5 = </ThemedText>
-        </ThemedView>
-        <ThemedView style={styles.outputBox}>
-          <ThemedText>{number5s !== '' ? parseInt(number5s) * 5 : 0}</ThemedText>
-        </ThemedView>
-      </ThemedView>
-      <ThemedView style={styles.billRowContainer}>
-        <ThemedView style={styles.inputBox}>
-          <TextInput 
-              keyboardType='numeric' 
-              value={number1s} 
-              onChangeText={text => setNumber1s(text)} 
-              placeholder="How many $1 bills?"
-              placeholderTextColor='grey'
-          />
-        </ThemedView>
-        <ThemedView style={styles.multiplyBox}>
-          <ThemedText>x $1 = </ThemedText>
-        </ThemedView>
-        <ThemedView style={styles.outputBox}>
-          <ThemedText>{number1s !== '' ? number1s : 0}</ThemedText>
-        </ThemedView>
-      </ThemedView>
-      <ThemedView style={styles.billRowContainer}>
-        {(() => {
-        switch (selectedItem) {
-          case 'safe':
-            return (
-              <ThemedView>
-              <ThemedView style={styles.inputBox}>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? "padding" : "height"}
+      style={{ flex: 1}}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <ParallaxScrollView
+          headerBackgroundColor={{ light: '#D0D0D0', dark: '#38761d' }}
+          headerImage={
+            <IconSymbol
+              size={310}
+              color="#b6d7a8"
+              name="dollarsign"
+              style={styles.headerImage}
+            />
+          }>
+          <ThemedView style={styles.titleContainer}>
+            <ThemedText type="title">Count the Register</ThemedText>
+          </ThemedView>
+          <ThemedText>This page will include the functionality to help you count your register. Please select if you are counting the register or safe and enter the counts of bills and coins below:</ThemedText>
+          <ThemedView style={styles.billRowContainer}>
+            <SelectDropdown
+              data={itemToCount}
+              onSelect={(selectedItem, index) => {
+                console.log(selectedItem, index);
+                setSelectedItem(selectedItem.title);
+              }}
+              renderButton={(selectedItem, isOpen) => {
+                return (
+                  <ThemedView style={styles.dropdownButtonStyle}>
+                    <ThemedText style={styles.dropdownButtonTxtStyle}>{selectedItem !== null ? selectedItem.title : 'What are you counting? (Tap Me)'}</ThemedText>
+                  </ThemedView>
+                );
+              }}
+              renderItem={(item, index, isSelected) => {
+                return (
+                  <ThemedView
+                    style={{
+                      ...styles.dropdownItemStyle,
+                      ...(isSelected && {backgroundColor: '#D2D9DF'}),
+                    }}>
+                    <ThemedText style={styles.dropdownItemTxtStyle}>{item.title}</ThemedText>
+                  </ThemedView>
+                );
+              }}
+              dropdownStyle={styles.dropdownMenuStyle}
+            />
+          </ThemedView>
+          <ThemedView style={styles.billRowContainer}>
+          <ThemedView style={styles.inputWrapper}>
+              <TextInput 
+                  style={styles.inputBox} 
+                  keyboardType='numeric' 
+                  value={number20s} 
+                  onChangeText={text => setNumber20s(text)} 
+                  placeholder="How many $20 bills?"
+                  placeholderTextColor='grey'
+              />
+            </ThemedView>
+            <ThemedView style={styles.multiplyBox}>
+              <ThemedText>x $20 = </ThemedText>
+            </ThemedView>
+            <ThemedView style={styles.outputBox}>
+              <ThemedText>{number20s !== '' ? parseInt(number20s) * 20 : 0}</ThemedText>
+            </ThemedView>
+          </ThemedView>
+          <ThemedView style={styles.billRowContainer}>
+            <ThemedView style={styles.inputWrapper}>
+              <TextInput 
+                  style={styles.inputBox} 
+                  keyboardType='numeric' 
+                  value={number10s} 
+                  onChangeText={text => setNumber10s(text)} 
+                  placeholder="How many $10 bills?"
+                  placeholderTextColor='grey'
+              />
+            </ThemedView>
+            <ThemedView style={styles.multiplyBox}>
+              <ThemedText>x $10 = </ThemedText>
+            </ThemedView>
+            <ThemedView style={styles.outputBox}>
+              <ThemedText>{number10s !== '' ? parseInt(number10s) * 10 : 0}</ThemedText>
+            </ThemedView>
+          </ThemedView>
+          <ThemedView style={styles.billRowContainer}>
+          <ThemedView style={styles.inputWrapper}>
+              <TextInput 
+                  style={styles.inputBox} 
+                  keyboardType='numeric' 
+                  value={number5s} 
+                  onChangeText={text => setNumber5s(text)} 
+                  placeholder="How many $5 bills?"
+                  placeholderTextColor='grey'
+              />
+            </ThemedView>
+            <ThemedView style={styles.multiplyBox}>
+              <ThemedText>x $5 = </ThemedText>
+            </ThemedView>
+            <ThemedView style={styles.outputBox}>
+              <ThemedText>{number5s !== '' ? parseInt(number5s) * 5 : 0}</ThemedText>
+            </ThemedView>
+          </ThemedView>
+          <ThemedView style={styles.billRowContainer}>
+            <ThemedView style={styles.inputWrapper}>
+              <TextInput 
+                  style={styles.inputBox} 
+                  keyboardType='numeric' 
+                  value={number1s} 
+                  onChangeText={text => setNumber1s(text)} 
+                  placeholder="How many $1 bills?"
+                  placeholderTextColor='grey'
+              />
+            </ThemedView>
+            <ThemedView style={styles.multiplyBox}>
+              <ThemedText>x $1 = </ThemedText>
+            </ThemedView>
+            <ThemedView style={styles.outputBox}>
+              <ThemedText>{number1s !== '' ? number1s : 0}</ThemedText>
+            </ThemedView>
+          </ThemedView>
+          <ThemedView style={styles.billRowContainer}>
+            {selectedItem === "register" && (
+              <ThemedView style={styles.billRowContainer}>
+              <ThemedView style={styles.inputWrapper}>
                 <TextInput 
+                    style={styles.inputBox} 
                     keyboardType='numeric' 
                     value={numberCent} 
                     onChangeText={text => setNumberCent(text)} 
@@ -164,41 +206,80 @@ export default function TabFourScreen() {
                 <ThemedText>{numberCent}</ThemedText>
               </ThemedView>
               </ThemedView>
-            );
-          case 'register':
-            return (
-              <ThemedView>
-              <ThemedView style={styles.inputBox}>
-                <TextInput 
-                    keyboardType='numeric' 
-                    value={numberCent} 
-                    onChangeText={text => setNumberCent(text)} 
-                    placeholder="How much in coins?"
-                    placeholderTextColor='grey'
-                />
+            )}
+            {selectedItem === "safe" && (
+              <ThemedView style={styles.coinRowContainer}>
+                <ThemedView style={styles.billRowContainer}>
+                  <ThemedView style={styles.inputWrapper}>
+                    <TextInput 
+                        style={styles.inputBox} 
+                        keyboardType='numeric' 
+                        value={numberQs} 
+                        onChangeText={text => setNumberQ(text)} 
+                        placeholder="Rolls of Quarters?"
+                        placeholderTextColor='grey'
+                    />
+                  </ThemedView>
+                  <ThemedView style={styles.multiplyBox}>
+                    <ThemedText>x $10 = </ThemedText>
+                  </ThemedView>
+                  <ThemedView style={styles.outputBox}>
+                    <ThemedText>{numberQs !== '' ? parseInt(numberQs) * 10 : 0}</ThemedText>
+                  </ThemedView>
+                </ThemedView>
+                <ThemedView style={styles.billRowContainer}>
+                  <ThemedView style={styles.inputWrapper}>
+                    <TextInput 
+                        style={styles.inputBox} 
+                        keyboardType='numeric' 
+                        value={numberDs} 
+                        onChangeText={text => setNumberD(text)} 
+                        placeholder="Rolls of Dimes?"
+                        placeholderTextColor='grey'
+                    />
+                  </ThemedView>
+                  <ThemedView style={styles.multiplyBox}>
+                    <ThemedText>x $5 = </ThemedText>
+                  </ThemedView>
+                  <ThemedView style={styles.outputBox}>
+                    <ThemedText>{numberDs !== '' ? parseInt(numberDs) * 5 : 0}</ThemedText>
+                  </ThemedView>
+                </ThemedView>
+                <ThemedView style={styles.billRowContainer}>
+                  <ThemedView style={styles.inputWrapper}>
+                    <TextInput 
+                        style={styles.inputBox} 
+                        keyboardType='numeric' 
+                        value={numberNs} 
+                        onChangeText={text => setNumberN(text)} 
+                        placeholder="Rolls of Nickels?"
+                        placeholderTextColor='grey'
+                    />
+                  </ThemedView>
+                  <ThemedView style={styles.multiplyBox}>
+                    <ThemedText>x $2 = </ThemedText>
+                  </ThemedView>
+                  <ThemedView style={styles.outputBox}>
+                    <ThemedText>{numberNs !== '' ? parseInt(numberNs) * 2 : 0}</ThemedText>
+                  </ThemedView>
+                </ThemedView>
               </ThemedView>
-              <ThemedView style={styles.outputBox}>
-                <ThemedText>{numberCent}</ThemedText>
-              </ThemedView>
-              </ThemedView>
-            );
-          default:
-            return (
+            )}
+            {selectedItem === '' && (
               <ThemedView style={styles.unknownCountItem}>
                 <ThemedText>Please choose what you are counting above</ThemedText>
               </ThemedView>
-            );
-        };
-      }
-      )()}
-      </ThemedView>
-      <ThemedView style={styles.totalBox}>
-        <ThemedText>Total = ${total.toFixed(2)}</ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.calculateBox}>
-        <Button title="Calculate!" onPress={calculateTotal}/>
-      </ThemedView>
-    </ParallaxScrollView>
+            )}
+          </ThemedView>
+          <ThemedView style={styles.totalBox}>
+            <ThemedText>Total = ${total.toFixed(2)}</ThemedText>
+          </ThemedView>
+          <ThemedView style={styles.calculateBox}>
+            <Button title="Calculate!" onPress={calculateTotal}/>
+          </ThemedView>
+        </ParallaxScrollView>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -218,6 +299,11 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between'
   },
+  coinRowContainer: {
+    flex: 1,
+    justifyContent: 'space-around',
+    gap: 13
+  },
   unknownCountItem:{
     flex: 1,
     flexDirection: 'row',
@@ -226,12 +312,15 @@ const styles = StyleSheet.create({
     backgroundColor: 'maroon',
     borderWidth: 2,
   },
-  inputBox: {
+  inputWrapper: {
     width: 150,
     backgroundColor: 'white',
     borderWidth: 2,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  inputBox:{
+    
   },
   multiplyBox: {
     justifyContent: 'center',
