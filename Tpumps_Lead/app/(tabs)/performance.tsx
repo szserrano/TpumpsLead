af6260dc017b96d6e388c4226f04317b2d7c9700
szserrano <sjzserrano@gmail.com>
2025-03-13 17:03:32 +0000

    Fix minutes display on timer

--- Tpumps_Lead/app/(tabs)/performance.tsx
+++ Tpumps_Lead/app/(tabs)/performance.tsx
@@ -1,4 +1,5 @@
-import { StyleSheet, Image, Platform } from 'react-native';
+import { StyleSheet, Image, Platform, Button } from 'react-native';
+import { useState, useEffect } from 'react';
 
 import { Collapsible } from '@/components/Collapsible';
 import { ExternalLink } from '@/components/ExternalLink';
@@ -8,6 +9,36 @@ import { ThemedView } from '@/components/ThemedView';
 import { IconSymbol } from '@/components/ui/IconSymbol';
 
 export default function TabSixScreen() {
+  const [isActive, setIsActive] = useState(false);
+  const [time, setTime] = useState(0);
+  const [sec, setSec] = useState(0);
+  const [min, setMin] = useState(0);
+  
+  useEffect(() => {
+    if(isActive){
+      let intervalID = setInterval(() => {
+        setTime(time + 1)
+      }, 1000)
+      
+      
+      setSec((time % 60));
+      let remainSeconds = sec % 60;
+      let minutes = (time - remainSeconds) / 60; 
+      console.log(minutes);
+      setMin(minutes);
+
+      return () => clearInterval(intervalID);
+    }
+  });
+
+  const handleToggle = () => {
+    setIsActive(!isActive);
+  }
+
+  const clearTimer = () => {
+    setTime(0);
+  }
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#8c0d68' }}
@@ -22,7 +53,11 @@ export default function TabSixScreen() {
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Performance</ThemedText>
       </ThemedView>
-      <ThemedText>This page will provide you a graph of count times as well as your trends of time to close as you continue to use this app.</ThemedText>
+      <ThemedText>This page will provide you a graph of count times as well as your trends of time to close as you continue to use this app. It also gives you the ability to time yourself for counting and sending your report.</ThemedText>
+      <ThemedView style={styles.timerContainer}>
+        <ThemedText type='subtitle'>{min} Minutes, {sec} Seconds </ThemedText>
+      </ThemedView>
+      <Button title={isActive ? 'Pause Timer' : 'Resume Timer'} onPress={handleToggle}/>
     </ParallaxScrollView>
   );
 }
@@ -36,6 +71,11 @@ const styles = StyleSheet.create({
   },
   titleContainer: {
     flexDirection: 'row',
+    gap: 8,
+  },
+  timerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
     gap: 8,
   },
 });
